77b7e68a1950004c2a6ce9b213d3719a6a5ed309
acbharat <acbharat@gmail.com>
2016-06-23 12:32:09 +0000

    boot

--- tests/acceptance/_bootstrap.php
+++ tests/acceptance/_bootstrap.php
@@ -1,2 +1,7 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+
+\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
+
+date_default_timezone_set('Asia/Dubai');
